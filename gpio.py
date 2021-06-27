f210ad1bf6f7cb096b8b179eadb79176d13b516b
ConsuminHelium <32295016+Wizface@users.noreply.github.com>
2021-06-27 18:23:18 +0200

    Add files via upload

--- gpio.py
+++ gpio.py
@@ -1,11 +1,8 @@
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
-
-def button_callback(channel):
-    print("Button was pushed!")
-
-
-GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setwarnings(True) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
-GPIO.add_event_detect(3,GPIO.RISING,callback=button_callback) # Setup event on pin 10 rising edge
+while True: # Run forever
+    if GPIO.input(10) == GPIO.LOW:
+        print("Button was pushed!")
