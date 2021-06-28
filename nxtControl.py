e7a1dcd33fcbeb0cbd55b42e9c2ced47a475ecf4
ConsuminHelium <32295016+Wizface@users.noreply.github.com>
2021-06-28 11:11:12 +0200

    Add files via upload

--- nxtControl.py
+++ nxtControl.py
@@ -21,7 +21,32 @@ if '--verbose' in sys.argv or '--debug' in sys.argv:
     print('debug = True')
 
 b = None
-b = nxt.locator.find_one_brick()
+try:
+    print('Find brick...', flush=True)
+    b = nxt.locator.find_one_brick(debug=True)
+    name, host, signal_strength, user_flash = b.get_device_info()
+    print('NXT brick name: %s' % name)
+    print('Host address: %s' % host)
+    print('Bluetooth signal strength: %s' % signal_strength)
+    print('Free user flash: %s' % user_flash)
+    prot_version, fw_version = b.get_firmware_version()
+    print('Protocol version %s.%s' % prot_version)
+    print('Firmware version %s.%s' % fw_version)
+    millivolts = b.get_battery_level()
+    print('Battery level %s mV' % millivolts)
+    print('Play test sound...', end='', flush=True)
+    b.play_tone_and_wait(300, 50)
+    b.play_tone_and_wait(400, 50)
+    b.play_tone_and_wait(500, 50)
+    b.play_tone_and_wait(600, 50)
+    print('done')
+except:
+    print("Error while running test:")
+    traceback.print_tb(sys.exc_info()[2])
+    print(str(sys.exc_info()[1]))
+    if b in locals():
+        b.sock.close()
+
 
 print("AIDS")
 
